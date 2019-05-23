ca30d1ea86f95e39b2e980d054cf8322ef97e5db
oohitoo <oohitoo@naver.com>
2019-05-23 16:40:54 +0900

    190523-Socket-v002

---
+++ BusanIT_JSP_Project/WebContent/broadcast.jsp
@@ -0,0 +1,50 @@
+<%@ page contentType="text/html; charset=EUC-KR"%>
+<%
+	request.setCharacterEncoding("EUC-KR");
+%>
+<body>
+	<fieldset>
+		<textarea id="messageWindow" rows="10" cols="50" readonly="true"></textarea>
+		<br /> <input id="messid" type="text" size="8" value="어드민" /> <input
+			id="inputMessage" type="text" /> <input type="submit" value="send"
+			onclick="send()" />
+	</fieldset>
+</body>
+<script type="text/javascript">
+	/* 소켓 처리 해야할것
+		1. 가게별 세션을 담아서 처리
+		2. 보낼때 앞에 가게 이름 붙여서 보내기
+		   2 -1. 가게이름 보낼대 hidden 값에 셋팅해서 가져오기
+		3. 보낸 후 받을때 알람 처리
+	*/
+    
+	var textarea = document.getElementById("messageWindow");
+	var webSocket = new WebSocket('ws://localhost:80/BusanIT_JSP_Project/broadcasting');
+	var inputMessage = document.getElementById('inputMessage');
+	var id = document.getElementById('messid');
+
+	webSocket.onerror = function(event) {
+		onError(event)
+	};
+	webSocket.onopen = function(event) {
+		onOpen(event)
+	};
+	webSocket.onmessage = function(event) {
+		onMessage(event)
+	};
+	function onMessage(event) {
+		textarea.value += "상대 : " + event.data + "\n";
+	}
+	function onOpen(event) {
+		textarea.value += "연결 성공\n";
+	}
+	function onError(event) {
+		alert(event.data);
+	}
+	function send() {
+		textarea.value += "나 : " + inputMessage.value + "\n";
+		webSocket.send(id.value + ":" + inputMessage.value);
+		inputMessage.value = "";
+	}
+</script>
+</html>
--- BusanIT_JSP_Project/src/Broadsocket.java
+++ BusanIT_JSP_Project/src/Broadsocket.java
@@ -1,51 +1,88 @@
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import javax.servlet.http.HttpSession;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/broadcasting")
+/*@ServerEndpoint("/broadcasting")*/
+
+/* http session 추가 */
+@ServerEndpoint(value = "/broadcasting",configurator = GetHttpSessionConfigurator.class)
 public class Broadsocket {
 
-	private static Set<Session> clients = Collections
-			.synchronizedSet(new HashSet<Session>());
+	// 접속한 클라이언트 세션
+	private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+	//접속한 shop의 세션
+	private static Set<Session> shops = Collections.synchronizedSet(new HashSet<Session>());
+
+	// 서블릿 세션
+	private static Map<Session, EndpointConfig> configs = Collections.synchronizedMap(new HashMap<Session, EndpointConfig>());
 
 	@OnMessage
 	public void onMessage(String message, Session session) throws IOException {
 		System.out.println("메세지 "+ message); //메세지 가져옴
-		String shop = message.split(":")[0];
-		String ma = message.split(":")[1];
-		System.err.println(shop);
-		synchronized (clients) {
-			// Iterate over the connected sessions
-			// and broadcast the received message
-			// 연결된 세션을 반복 하여 수신된 메세지를 보낸다.
+		String[] shopname = message.split(":");
+		String a = shopname[0];
+		String b = shopname[1];
 
-			for (Session client : clients) {
-				if (!client.equals(session)) {
-					client.getBasicRemote().sendText(message);
+			synchronized (clients) {
+				// Iterate over the connected sessions
+				// and broadcast the received message
+				// 연결된 세션을 반복 하여 수신된 메세지를 보낸다.
+//				for (Session client : clients) {
+//					if (!client.equals(session)){ //클라이언트 session 과 안에 있는 세션값
+//						client.getBasicRemote().sendText(message);
+//					}
+//				}
+//				
+				for(Session shop : shops) {
+					System.out.println(a);
+					EndpointConfig config = configs.get(shop);
+					HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+					if(httpSession.getAttribute("shop").equals(a)) {
+						shop.getBasicRemote().sendText(b);	
+					}
+
 				}
 			}
 		}
-	}
 
 	@OnOpen
-	public void onOpen(Session session) {
+	public void onOpen(EndpointConfig config, Session session) {
 		// Add session to the connected sessions set
 		// 연결된 세션에 추가하기
-		System.out.println(session);
-		clients.add(session);
+		System.out.println(session);		
+		HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+		configs.put(session, config);
+		if (httpSession.getAttribute("shop") != null) {
+			shops.add(session);
+		} else {
+			clients.add(session);
+		}
 	}
 
 	@OnClose
 	public void onClose(Session session) {
 		// Remove session from the connected sessions set
 		// 연결이 끊기면 세션 삭제
-		clients.remove(session);
+		EndpointConfig config = configs.get(session);
+		HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+
+		if (httpSession.getAttribute("shop") != null) {
+			shops.remove(session);
+		} else {
+			clients.remove(session);
+		}
+		configs.remove(session);
 	}
 }
